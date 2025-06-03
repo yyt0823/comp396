460ca18a2c8b90b73f926553076756e62dc6836b
YYT <97915915+yyt0823@users.noreply.github.com>
2025-06-03 18:15:38 +0800

    app_init

---
+++ Value/Value/ValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValueApp.swift
+//  Value
+//
+//  Created by yyt on 2025/6/3.
+//
+
+import SwiftUI
+
+@main
+struct ValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Value/ValueTests/ValueTests.swift
@@ -0,0 +1,17 @@
+//
+//  ValueTests.swift
+//  ValueTests
+//
+//  Created by yyt on 2025/6/3.
+//
+
+import Testing
+@testable import Value
+
+struct ValueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
